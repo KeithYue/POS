7e43f7e80cf76f452912adaab06e6a7c138d9f9a
KeithYue <buaawangyue@gmail.com>
2014-03-05 17:25:20 +0800

    The coverage has been improved to 70 percent.

--- README.md
+++ README.md
@@ -10,3 +10,19 @@ Java development environment, and to practice unit testing of object-oriented pr
 * how to test the mehtod that can invoke System.exit(1), which can shut down the while JVM, including all the test cases.
 http://stackoverflow.com/questions/309396/java-how-to-test-methods-that-call-system-exit?lq=1
   * http://www.stefan-birkner.de/system-rules/index.html, use rule to solve this problem
+* mock System.in input, there is no check information about user information in the saleRecord function. Besides, we can invoke saleRecord() without using the register method
+* username and password is public attr
+* POS functional Test:
+  * normal input test
+  * has member ship / no membership
+  * cash enough / cash not enough, there is not any check for invalid cash such as negative cash
+* How to decide whether the saleRegister menthod is normally invoked.
+  
+#### Some guidelines
+* The purpose of test cases is to test if a method performs correctly. 
+As such, each test case MUST define its expected outputs/behavior. 
+The assert statements should reflect what are to be checked if the test is carried out manually.
+* Your test cases do not need to cover those branches that unconditionally/always lead to the execution of System.exit.
+* So in order to test some methods that require console inputs, you need to simulate user interactions by 
+redirecting system standard input to your own input streams. 
+For example, you can redirect System.in to some string
--- src/test/POSConsoleModeTest.java
+++ src/test/POSConsoleModeTest.java
@@ -1,14 +1,60 @@
 package test;
 
 import static org.junit.Assert.*;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import POS.POS;
 
 public class POSConsoleModeTest {
+	private String userName = "zhulin";
+	private String passWord = "zhulin";
+	private String userNameInput = "zhulin\n" +
+			"zhulin\n";
+
+	private String saleRecordInput = "1\n" +
+			"y\n" +
+			"ID001\n" +
+			"2\n" +
+			"c\n" +
+			"2\n" +
+			"2\n";
+	
+	private String saleWithNoMembership = "1\n" +
+			"n\n" +
+			"ID001\n" +
+			"2\n" +
+			"c\n" +
+			"2\n" +
+			"2\n";
+	private String saleWithNotEnoughCash = "1\n" +
+			"n\n" +
+			"ID001\n" +
+			"2\n" +
+			"c\n" +
+			"0.5\n" +
+			"3\n" +
+			"2\n";
+	
+	private String saleWithInvalidCash = "1\n" +
+			"y\n" +
+			"ID001\n" +
+			"2\n" +
+			"c\n" +
+			"-2\n" +
+			"-1\n" +
+			"2\n" +
+			"2\n";
+
+	@Rule
+	public final TextFromStandardInputStream systemInMock = emptyStandardInputStream();
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -20,10 +66,54 @@ public class POSConsoleModeTest {
 
 	@Before
 	public void setUp() throws Exception {
+		POS.getInst().batchMode = false;
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-
+	
+	@Test
+	public void initTest(){
+		POS.getInst().init();
+	}
+	
+	@Test
+	public void saleRegisterTest(){
+		setUpWithUser();
+		// provide the sales record input
+		systemInMock.provideText(saleRecordInput);
+		POS.getInst().saleRegister();
+	}
+	
+	@Test
+	public void saleRegisterNoMemberCard(){
+		setUpWithUser();
+		// provide user not a membership
+		systemInMock.provideText(saleWithNoMembership);
+		POS.getInst().saleRegister();
+	}
+	
+	@Test 
+	public void saleRegisterWithoutEnoughCash(){
+		setUpWithUser();
+		// provider a user which has not enough money for the pencil
+		systemInMock.provideText(saleWithNotEnoughCash);
+		POS.getInst().saleRegister();
+	}
+	
+	@Test
+	public void saleRegisterWithInvalidCash(){
+		setUpWithUser();
+		// provide a user with invalid cash number, the cash number is negative
+		systemInMock.provideText(saleWithInvalidCash);
+		POS.getInst().saleRegister();
+	}
+	
+	private void setUpWithUser(){
+		POS.getInst().init();
+		// set the user name and password
+		POS.getInst().userName = userName;
+		POS.getInst().password = passWord;
+	}
 }
